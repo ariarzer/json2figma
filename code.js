05665008dc564bf293f12a0ceec46391e936ed28
Sofi Valitova <ariarzer@gmail.com>
2021-07-05 16:43:46 +0300

    init commit: generate palettes

---
+++ code.js
@@ -0,0 +1,29 @@
+function ahex2rgbaArr(color) {
+    const full = color.length === 9 ? color.slice(1) : 'FF' + color.slice(1);
+    const result = [];
+    for (let i = 0; i < 8; i += 2) {
+        result.push(parseInt(full.slice(i, i + 2), 16) / 255);
+    }
+    return result;
+}
+figma.showUI(__html__);
+figma.ui.onmessage = msg => {
+    if (msg.type === 'create-palette') {
+        Object.keys(msg.palette).forEach(colorName => {
+            const style = figma.createPaintStyle();
+            const [a, r, g, b] = ahex2rgbaArr(msg.palette[colorName]);
+            const solidPaint = {
+                type: "SOLID",
+                color: {
+                    r: r,
+                    g: g,
+                    b: b
+                },
+                opacity: a,
+            };
+            style.name = `${msg.name}/${colorName}`;
+            style.paints = [solidPaint];
+        });
+    }
+    figma.closePlugin();
+};
